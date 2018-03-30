9bfe464fa585b19e83c33fadb0b292e3d6e02bdb
Jeyaganeshan Jeyatharsini <JJeyatharsini@cambio.se>
2018-03-30 13:25:08 +0530

    Getting a single resource

--- src/main/java/com/jeya/springboot/topic/TopicController.java
+++ src/main/java/com/jeya/springboot/topic/TopicController.java
@@ -3,6 +3,7 @@ package com.jeya.springboot.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,12 @@ public class TopicController {
 										// annotation "@RestController"
 	{
 		return topicService.getAllTopics();
+	}
+	
+	@RequestMapping("/topics/{foo}") // if names are same no need to specify it in parenthesis
+	public Topic getTopic(@PathVariable("foo") String id)
+	{
+		return topicService.getTopic(id);
 	}
 }
 
